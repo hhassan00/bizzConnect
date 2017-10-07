6d8399291ab01baa83e17e73650b1198fcbc384d
hassan hafeez <hhassanhafeez7@gmail.com>
2017-10-07 21:41:44 +0500

    application posts lets push github

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -28,7 +28,7 @@
 			<td>{{$post->id}}</td>
 			<td><img height="50" src="{{$post->photo ? $post->photo->file : 'https://i.pinimg.com/736x/af/99/5d/af995d61b255bfd53bbdbfa343451949.jpg'}}" alt=""></td>
 			<td>{{$post->user->name}}</td>
-			<td>{{$post->category_id}}</td>
+			<td>{{$post->category ? $post->category->name : 'Un-categorized'}}</td>
 			<td>{{$post->title}}</td>
 			<td>{{$post->body}}</td>
 			<td>{{$post->created_at->diffForHumans()}}</td>
